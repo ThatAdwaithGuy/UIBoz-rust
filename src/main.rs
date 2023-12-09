48ec75abb16954492a782562533134961fd220d3
Adwaith <adwaith.3110@gmail.com>
2023-12-09 13:42:59 +0530

    bfix&feat: fixed parse_opts and removed its errors
    Fixed unexpected error by parse_opts and removed it Result type by ignoring
    the duplicate values.

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,47 @@
 use draw_boz::opts::{self, Opts};
+
+use crate::draw_boz::opts::{parse_opts, Colors};
 mod animations;
 mod draw_boz;
 mod errors;
 mod event_loop;
+
 // The Idea is to have
 
-/*
-fn make_lists_equal_length(list1: Vec<char>, list2: Vec<char>) -> (Vec<char>, Vec<char>) {
-    let (bigger_list, mut smaler_list) = if list1.len() > list2.len() {
-        (list1, list2)
-    } else if list1.len() < list2.len() {
-        (list2, list1)
-    } else {
-        (list1, list2)
-    };
-
-    let padding = bigger_list.len() - smaler_list.len();
-    let add_pad = vec![' '; padding];
-    smaler_list.extend(add_pad.iter());
-
-    let output = (bigger_list, smaler_list);
-    output
-}
-
-pub fn overlay_2_str<'a>(str1: &'a str, str2: &'a str) -> Result<String, TextError> {
-    let (bigger_str, smaler_str) = if str1.len() > str2.len() {
-        (str1, str2)
-    } else if str1.len() < str2.len() {
-        (str2, str1)
-    } else {
-        (str1, str2)
-    };
-
-    let mut bigger_list: Vec<char> = bigger_str.chars().collect::<Vec<char>>();
-    let mut smaler_list: Vec<char> = smaler_str.chars().collect::<Vec<char>>();
-    (bigger_list, smaler_list) = make_lists_equal_length(bigger_list.clone(), smaler_list.clone());
-
-    let mut output_list: Vec<char> = vec![];
-
-    for i in 0..bigger_list.len() {
-        if bigger_list[i] != ' ' && smaler_list[i] != ' ' {
-            return Err(TextError::TextOverlayed(
-                bigger_str.to_owned(),
-                smaler_str.to_owned(),
-            ));
-        }
-    }
-
-    for i in 0  ..bigger_list.len() {
-        if bigger_list[i] == ' ' && smaler_list[i] == ' ' {
-            output_list.push(' ');
-        } else if bigger_list[i] != ' ' && smaler_list[i] == ' ' {
-            output_list.push(bigger_list[i]);
-        } else if bigger_list[i] == ' ' && smaler_list[i] != ' ' {
-            output_list.push(smaler_list[i]);
-        } else if bigger_list[i] != ' ' && smaler_list[i] != ' ' {
-            output_list.push(bigger_list[i]);
-        }
-    }
-
-    Ok(String::from_iter(output_list))
-}
-*/
-
-fn main() -> Result<(), errors::UIBozErrors> {
-    let a = draw_boz::Boz::new(
-        vec![draw_boz::Text {
+fn main() {
+    let text_data = vec![
+        draw_boz::Text {
             text: "hello",
             line_number: 5,
             column: 10,
             opts: vec![Opts::Bold(Some(true))],
-        }],
-        true,
-        16,
-        52,
+        },
+        draw_boz::Text {
+            text: "hell",
+            line_number: 10,
+            column: 10,
+            opts: vec![
+                Opts::ForeColor(Some(Colors::Red)),
+                Opts::ForeColor(Some(Colors::Black)),
+            ],
+        },
+    ];
+
+    println!(
+        "{:?}",
+        parse_opts(vec![
+            Opts::ForeColor(Some(Colors::Red)),
+            Opts::BackGroundColor(Some(Colors::Black)),
+            Opts::ForeColor(Some(Colors::Red)),
+            Opts::BackGroundColor(Some(Colors::Black)),
+        ])
     );
 
-    a.render_string();
-
-    Ok(())
+    let a = draw_boz::Boz::new(text_data, true, 16, 52);
+    // let av = match a.render_string() {
+    //     Ok(val) => println!("{:?}", val),
+    //     Err(err) => {
+    //         panic!("{:?}", err)
+    //     }
+    // };
 }
