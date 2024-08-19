6ecac96b1153bfdb1cc8f3887f8c90e01794c76e
Adwaith <adwaith.3110@gmail.com>
2024-08-19 19:39:22 +0530

    deletion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod errors;
 pub mod raw_window;
 pub mod style;
 pub mod sub_win;
+pub mod window;
 fn main() -> Result<(), errors::TextError> {
     let texts = vec![];
     let window = sub_win::NestedWindow::new(texts, 12, 12, raw_window::TypeOfBorder::CurvedBorders);
