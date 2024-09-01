d6621c6448511ae67f4db99d9d4493b5cf4c6cf8
Adwaith <adwaith.3110@gmail.com>
2024-09-01 11:50:45 +0530

    before switch

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,14 @@
 use thiserror::Error;
 
+use crate::sub_win::SubWindow;
+
 #[derive(Error, Debug)]
 pub enum TextError {
-    #[error("\x1b[91mERROR\x1b[0m: looks like {0} and {1} are overlaping with each other.")]
+    #[error("\x1b[91mERROR\x1b[0m: Max depth exceeded for sub window {0:#?}")]
+    DepthLimitExceeded(SubWindow),
+    #[error("\x1b[91mERROR\x1b[0m: looks like {0} and {1} are overlapping with each other.")]
     TextOverlayed(String, String),
-    #[error("\x1b[91mERROR\x1b[0m: {0} is occpuying another text.")]
+    #[error("\x1b[91mERROR\x1b[0m: {0} is occupying another text.")]
     DuplicateText(String),
     #[error("\x1b[91mERROR\x1b[0m: {0} is leaving the bounds of the screen.")]
     LeftBounds(String),
--- src/style/mod.rs
+++ src/style/mod.rs
@@ -207,6 +207,5 @@ pub fn parse_text_style(lst: Rc<[TextStyle]>) -> String {
             TextStyle::Hide(false) => output_string.push_str("\x1b[022m"),
         }
     }
-    dbg!(&output_string);
     output_string
 }
