ffc0bdab70457d6b6a170e19322f4989a3681fde
Adwaith <adwaith.3110@gmail.com>
2024-10-01 12:38:41 +0530

    git is not ignoring

--- find_size.sh
+++ find_size.sh
@@ -7,7 +7,9 @@ if [ ! -d "src" ]; then
 fi
 
 # Count lines recursively and sum them up
-total_lines=$(find src -type f | xargs wc -l | tail -n 1 | awk '{print $1}')
+total_lines_src=$(find src -type f | xargs wc -l | tail -n 1 | awk '{print $1}')
 
+total_lines_creates=$(find crates -type f | xargs wc -l | tail -n 1 | awk '{print $1}')
 # Output the result
+total_lines=$(($total_lines_src+$total_lines_creates))
 echo "Total lines in src directory: $total_lines"
